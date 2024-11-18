40c2248aea816b6c3acbe2ecc78ac0bc13a01910
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-11-18 14:45:17 +0700

    add table component to page

--- src/app/(authenticated)/product/page.tsx
+++ src/app/(authenticated)/product/page.tsx
@@ -1,7 +1,33 @@
+"use client";
 import React from "react";
+import { useProductQuery } from "./_hooks/useProductQuery";
+import TableData from "./_components/TableData/TableData";
+import { Button, Input } from "antd";
+import { SortAscendingOutlined, SearchOutlined } from "@ant-design/icons";
 
 const ListPage = () => {
-  return <div>ListPage</div>;
+  const { data } = useProductQuery();
+
+  return (
+    <div>
+      <div className="mb-3 flex items-center gap-x-2">
+        <Button
+          variant="outlined"
+          color="default"
+          icon={<SortAscendingOutlined />}
+          iconPosition="start"
+        >
+          Sort
+        </Button>
+        <Input
+          size="middle"
+          placeholder="search"
+          prefix={<SearchOutlined />}
+        ></Input>
+      </div>
+      <TableData data={data?.data} />
+    </div>
+  );
 };
 
 export default ListPage;
