e829e952588ef3a591aaeba2b71d04bd137ff720
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-11-19 17:46:33 +0700

    add callback to useProductQuery hooks for error handling

--- src/app/(authenticated)/product/_hooks/useProductQuery.ts
+++ src/app/(authenticated)/product/_hooks/useProductQuery.ts
@@ -2,11 +2,22 @@
 import { axiosInstance } from "@/libs/axios";
 import { useQuery } from "@tanstack/react-query";
 
-export const useProductQuery = () => {
+type useProductQueryParamsType = {
+  onError?: () => void;
+};
+
+export const useProductQuery = ({ onError }: useProductQueryParamsType) => {
   return useQuery({
     queryFn: async () => {
-      const resp = await axiosInstance("/products");
-      return resp;
+      try {
+        const resp = await axiosInstance("/products");
+        return resp;
+      } catch (error) {
+        if (onError) {
+          onError();
+        }
+      }
+      return null;
     },
     queryKey: ["listProduct"],
     refetchOnWindowFocus: false,
