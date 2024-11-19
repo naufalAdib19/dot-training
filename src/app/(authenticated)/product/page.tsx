27e19e42ed9bdd6049f76c9f452e30ab7576a52c
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-11-19 17:49:14 +0700

    adjust product page

--- src/app/(authenticated)/product/page.tsx
+++ src/app/(authenticated)/product/page.tsx
@@ -1,31 +1,65 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useProductQuery } from "./_hooks/useProductQuery";
 import TableData from "./_components/TableData/TableData";
-import { Button, Input } from "antd";
-import { SortAscendingOutlined, SearchOutlined } from "@ant-design/icons";
+import { Button, Input, Typography } from "antd";
+import {
+  SortAscendingOutlined,
+  SearchOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
+import { useRouter } from "next/navigation";
+import { message } from "antd";
+
+const { Title } = Typography;
 
 const ListPage = () => {
-  const { data } = useProductQuery();
+  const { data, isError } = useProductQuery({
+    onError: () => {
+      messageApi.open({
+        type: "error",
+        content: "Something wrong happen during get the data from server",
+      });
+    },
+  });
+  const router = useRouter();
+  const [messageApi, contextHolder] = message.useMessage();
 
   return (
     <div>
-      <div className="mb-3 flex items-center gap-x-2">
+      {contextHolder}
+      <div className="flex flex-col md:flex-row md:items-center justify-between mb-[16px]">
+        <Title level={2}>Material</Title>
         <Button
-          variant="outlined"
-          color="default"
-          icon={<SortAscendingOutlined />}
-          iconPosition="start"
+          color="primary"
+          size="large"
+          variant="solid"
+          icon={<PlusOutlined />}
+          onClick={() => {
+            router.push("/product/create");
+          }}
         >
-          Sort
+          Create Material
         </Button>
-        <Input
-          size="middle"
-          placeholder="search"
-          prefix={<SearchOutlined />}
-        ></Input>
       </div>
-      <TableData data={data?.data} />
+      <div className="bg-white p-[24px] rounded-lg">
+        <div className="mb-3 flex items-center gap-x-2">
+          <Button
+            variant="outlined"
+            color="default"
+            icon={<SortAscendingOutlined />}
+            iconPosition="start"
+          >
+            Sort
+          </Button>
+          <Input
+            size="middle"
+            placeholder="search"
+            prefix={<SearchOutlined />}
+          ></Input>
+        </div>
+        <TableData data={data?.data} />
+      </div>
     </div>
   );
 };
