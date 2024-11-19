7a60107a01d193c744bd86c2012727ab5cb7eee9
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-11-19 17:45:58 +0700

    create custom hooks for create data

---
+++ src/app/(authenticated)/product/_hooks/useCreateProduct.ts
@@ -0,0 +1,25 @@
+"use client";
+import { axiosInstance } from "@/libs/axios";
+import { useMutation } from "@tanstack/react-query";
+import { listProductType } from "../_types/listProductType";
+
+type useCreateProductParamsType = {
+  onSuccess: () => void;
+  onError?: () => void;
+};
+
+export const useCreateProduct = ({
+  onSuccess,
+  onError,
+}: useCreateProductParamsType) => {
+  return useMutation({
+    mutationFn: async (data: listProductType) => {
+      const resp = await axiosInstance.post("/products", {
+        data,
+      });
+      return resp;
+    },
+    onSuccess,
+    onError,
+  });
+};
