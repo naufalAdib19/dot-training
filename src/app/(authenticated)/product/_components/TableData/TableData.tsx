3469876aa37d9f4eba47a520d3d3626240c7b321
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-11-20 12:34:33 +0700

    update table component, add delete product features

--- src/app/(authenticated)/product/_components/TableData/TableData.tsx
+++ src/app/(authenticated)/product/_components/TableData/TableData.tsx
@@ -1,73 +1,109 @@
 "use client";
-import React from "react";
-import { Button, Table } from "antd";
+import React, { useState } from "react";
+import { Button, Table, Modal } from "antd";
 import type { TableColumnsType, TableProps } from "antd";
 import { listProductType } from "../../_types/listProductType";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
-
-const column: TableColumnsType<listProductType> = [
-  {
-    title: "Code",
-    dataIndex: "code",
-  },
-  {
-    title: "Name",
-    dataIndex: "name",
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-  },
-  {
-    title: "Account Code",
-    dataIndex: "accountCode",
-  },
-  {
-    title: "Description",
-    dataIndex: "description",
-  },
-  {
-    title: "Status",
-    dataIndex: "status",
-    render: (val, record) => (
-      <p className="uppercase text-xs text-blue-500 border border-blue-500 bg-blue-50 w-fit px-2 py-1 rounded">
-        {val}
-      </p>
-    ),
-  },
-  {
-    title: "Action",
-    render: (_, record) => (
-      <span>
-        <EditOutlined
-          className="hover:cursor-pointer"
-          style={{ color: "blue", marginRight: "8px" }}
-        />
-        <DeleteOutlined
-          className="hover:cursor-pointer"
-          style={{ color: "red" }}
-        />
-      </span>
-    ),
-  },
-];
+import { useDeleteProduct } from "../../_hooks/useDeleteProduct";
+import { message } from "antd";
+import { useQueryClient } from "@tanstack/react-query";
 
 const rowSelection: TableProps<listProductType>["rowSelection"] = {
-  onChange: (selectedRowKeys: React.Key[], selectedRows: listProductType[]) => {
-    console.log(
-      `selectedRowKeys: ${selectedRowKeys}`,
-      "selectedRows: ",
-      selectedRows
-    );
-  },
   getCheckboxProps: (record: listProductType) => ({
     name: record.name,
   }),
 };
 
 const TableData = ({ data }: { data: listProductType[] }) => {
+  const [isModalShow, setIsModalShow] = useState<boolean>(false);
+  const queryClient = useQueryClient();
+  const [messageApi, contextHolder] = message.useMessage();
+  const [currentSelectedProduct, setCurrentSelectedProduct] = useState<
+    Record<string, any>
+  >({
+    name: "",
+    id: 0,
+  });
+  const { mutate, isPending } = useDeleteProduct({
+    onError: () => {
+      messageApi.open({
+        type: "error",
+        content: `Something wrong happen when delete ${currentSelectedProduct.name}`,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["listProduct"],
+      });
+      messageApi
+        .open({
+          type: "success",
+          content: `Success delete ${currentSelectedProduct.name}`,
+          duration: 1.2,
+        })
+        .then(() => {
+          setIsModalShow(false);
+        });
+    },
+  });
+  const column: TableColumnsType<listProductType> = [
+    {
+      title: "Code",
+      dataIndex: "code",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+    },
+    {
+      title: "Account Code",
+      dataIndex: "accountCode",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (val, record) => (
+        <p className="uppercase text-xs text-blue-500 border border-blue-500 bg-blue-50 w-fit px-2 py-1 rounded">
+          {val}
+        </p>
+      ),
+    },
+    {
+      title: "Action",
+      render: (val, record) => (
+        <div className="flex items-center gap-x-2">
+          <EditOutlined
+            className="hover:cursor-pointer"
+            style={{ color: "blue", marginRight: "8px" }}
+          />
+          |
+          <DeleteOutlined
+            className="hover:cursor-pointer ml-2"
+            style={{ color: "red" }}
+            onClick={() => {
+              setIsModalShow(true);
+              setCurrentSelectedProduct({
+                id: val.id,
+                name: val.name,
+              });
+            }}
+          />
+        </div>
+      ),
+    },
+  ];
+
   return (
     <div>
+      {contextHolder}
       <Table<listProductType>
         rowSelection={{ ...rowSelection }}
         columns={column}
@@ -79,6 +115,25 @@ const TableData = ({ data }: { data: listProductType[] }) => {
           pageSize: 5,
         }}
       />
+      <Modal
+        title="DELETE MATERIAL"
+        open={isModalShow}
+        okText="Delete"
+        okButtonProps={{
+          variant: "solid",
+          color: "danger",
+          icon: <DeleteOutlined />,
+          disabled: isPending,
+        }}
+        onOk={() => {
+          mutate(currentSelectedProduct.id);
+        }}
+        onCancel={() => {
+          setIsModalShow(false);
+        }}
+      >
+        Are you sure want to delete {currentSelectedProduct.name}?
+      </Modal>
     </div>
   );
 };
